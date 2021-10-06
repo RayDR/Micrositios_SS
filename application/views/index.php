7cb5f320ccb1ddeffbcfe545e737e85122c92916
Ray DR <ray.dr@hotmail.com>
2021-10-06 16:33:33 -0500

    Attachments en directorio

--- application/views/index.php
+++ application/views/index.php
@@ -158,14 +158,13 @@
 
     <div class="row justify-content-center">
     <?php foreach($elementos->directorio as $key => $directorio): ?>
-      <?php print_r($directorio->attachments); ?>
       <?php if($key == 0): ?>
         <div class="col-12 mb-5">
           <div class="row d-flex justify-content-center align-items-center mx-auto">
       <?php endif; ?>
       <div class="col-6 col-md-4 col-lg-3 d-flex align-items-stretch">
         <div class="card">
-          <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?> <?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments[0]->jsat_fname): '' ?><?php endif ?>" alt="Nombre" onerror="this.onerror=null; this.src = '<?= base_url('sources/img/favicon.png') ?>'" style="max-height: <?= ($key == 0)? 390: 310 ?>px;" />
+          <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?> <?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments->jsat_fname): '' ?><?php endif ?>" alt="Nombre" onerror="this.onerror=null; this.src = '<?= base_url('sources/img/favicon.png') ?>'" style="max-height: <?= ($key == 0)? 390: 310 ?>px;" />
           <div class="card-body my-3">
             <h6 class="card-title text-primary font-weight-bold"><?= $directorio->fullname ?></h6>
             <h6 class="font-weight-bold"><?= $directorio->job_title ?></h6>
