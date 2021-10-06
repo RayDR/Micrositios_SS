fcd1ed6be8121fa80415e2116f177ccbf7779b34
Ray DR <ray.dr@hotmail.com>
2021-10-06 16:35:58 -0500

    Attachments en directorio

--- application/views/index.php
+++ application/views/index.php
@@ -164,7 +164,7 @@
       <?php endif; ?>
       <div class="col-6 col-md-4 col-lg-3 d-flex align-items-stretch">
         <div class="card">
-          <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?> <?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments->jsat_fname): '' ?><?php endif ?>" alt="Nombre" onerror="this.onerror=null; this.src = '<?= base_url('sources/img/favicon.png') ?>'" style="max-height: <?= ($key == 0)? 390: 310 ?>px;" />
+          <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?> <?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments->jsat_fname): '' ?><?php endif ?>" alt="Nombre"  style="max-height: <?= ($key == 0)? 390: 310 ?>px;" />
           <div class="card-body my-3">
             <h6 class="card-title text-primary font-weight-bold"><?= $directorio->fullname ?></h6>
             <h6 class="font-weight-bold"><?= $directorio->job_title ?></h6>
