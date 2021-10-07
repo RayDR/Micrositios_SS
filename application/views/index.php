686b688c983055f18606a1840888c7d22c830122
Ray DR <ray.dr@hotmail.com>
2021-10-07 09:07:30 -0500

    Corrección de responsivo de directorio[A

--- application/views/index.php
+++ application/views/index.php
@@ -161,7 +161,7 @@
       <div class="row justify-content-center mb-3">
         <div class="col-6 col-md-6">
           <div class="card">
-            <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?><?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments->jsat_fname): '' ?><?php endif ?>" alt="Nombre" onerror="this.onerror=null; this.src = '<?= base_url('sources/img/favicon.png') ?>'" style="max-height: 400px; min-height: 150px;" />
+            <img class="card-img p-0" src="https://webcore.setab.gob.mx/static/persons/<?php if ($directorio->attachments): ?><?= array_key_exists('jsat_fname', $directorio->attachments)? ($directorio->attachments->jsat_fname): '' ?><?php endif ?>" alt="Nombre" onerror="this.onerror=null; this.src = '<?= base_url('sources/img/favicon.png') ?>'" style="max-height: 400px; min-height: 200px;" />
             <div class="card-body my-3">
               <h6 class="card-title text-primary font-weight-bold"><?= $directorio->fullname ?></h6>
               <h6 class="font-weight-bold"><?= $directorio->job_title ?></h6>
